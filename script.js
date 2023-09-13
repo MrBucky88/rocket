1ac1b6812e6336cd1e178671708d4a020a187fe3
Bucky88 <135864957+MrBucky88@users.noreply.github.com>
2023-09-13 18:55:32 +0200

    Finally added js!
    Was waiting for that so long, and it actually happenned. Wow

---
+++ script.js
@@ -0,0 +1,8 @@
+let notyfikacja = document.querySelector(".news");
+
+notyfikacja.addEventListener('click', function(){
+    prompt("Dzięki za zasubowanie newslettera! Wpisz mail na który chcesz go otrzymywać!");
+    alert("Pomyślnie przyjęty! Możliwe że już dziś coś otrzymasz ;)");
+});
+
+notyfikacja.style.color = 'white';
